302a76881ead7a31bc0bbd2d7ef1128f12fc5cf7
Mardie328 <tolayinka328@gmail.com>
2023-04-25 23:03:28 +0100

    Added new app updates

---
+++ src/components/AllMovieCards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+const AllMovieCards = () => {
+  return (
+    <div className="AllMovieCards-container">
+      <MovieCard />
+    </div>
+  );
+};
+
+export default AllMovieCards;

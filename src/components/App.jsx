698d9b49d81519519d8c8d9bc7deb21fe049e013
Mardie328 <tolayinka328@gmail.com>
2023-04-29 13:31:13 +0100

    Updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import Logo from "./Logo";
 import AllMovieCards from "./AllMovieCards";
-//import MovieCard from "./MovieCard";
 
 const App = () => {
   return (
     <div className="main-container">
       <Logo />
       <AllMovieCards />
-      {/* <MovieCard /> */}
     </div>
   );
 };
